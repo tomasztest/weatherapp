3fedecf4ce07bc739a69dcd0c595170af134ae59
tomasztest <tomaszkotzur@gmail.com>
2019-09-26 08:50:38 +0200

    Updates

---
+++ precache-manifest.e93e40ec5ae2d7af44f8820779e406d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2487a6e27d3eee25f00adf4b1c6f8bd6",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "f5f20cac2cfd4685a01f",
+    "url": "/weatherapp/static/css/main.72cfe24f.chunk.css"
+  },
+  {
+    "revision": "6a629aec1d098c0923e2",
+    "url": "/weatherapp/static/js/2.96cb5612.chunk.js"
+  },
+  {
+    "revision": "f5f20cac2cfd4685a01f",
+    "url": "/weatherapp/static/js/main.71f6cbfa.chunk.js"
+  },
+  {
+    "revision": "bf16f4863ae6c35078f6",
+    "url": "/weatherapp/static/js/runtime~main.c2cefd07.js"
+  }
+]);
